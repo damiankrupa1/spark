8e7c42be921f23108bd5b893e769ac4215040d94
Damian <krupa5643@gmail.com>
2024-08-16 11:45:23 +0200

    wip

--- typescript-version/src/plugins/pinia.ts
+++ typescript-version/src/plugins/pinia.ts
@@ -1,8 +1,13 @@
-import { createPinia } from 'pinia'
-import type { App } from 'vue'
+import { router } from "@/plugins/router/index";
+import { createPinia } from 'pinia';
+import type { App, markRaw } from 'vue';
 
-export const store = createPinia()
+export const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
 
 export default function (app: App) {
-  app.use(store)
+  app.use(pinia)
 }
--- typescript-version/src/store/userSession.ts
+++ typescript-version/src/store/userSession.ts
@@ -1,45 +1,56 @@
-import { axiosInstance } from '@/plugins/axios';
-import { loginModel, loginResponse, registerModel, registerResponse } from '@/types/user';
-import { defineStore } from 'pinia';
+import { axiosInstance } from '@/plugins/axios'
+import { loginModel, loginResponse, registerModel, registerResponse } from '@/types/user'
+import { AxiosResponse } from 'axios'
+import { defineStore } from 'pinia'
 
 const setLoginData = (loginData: registerResponse) => {
-  localStorage.setItem('user', JSON.stringify(loginData.user));
-  localStorage.setItem('token', JSON.stringify(loginData.tokens.access));
-  localStorage.setItem('tokens', JSON.stringify(loginData.tokens));
+  localStorage.setItem('user', JSON.stringify(loginData.user))
+  localStorage.setItem('token', JSON.stringify(loginData.tokens.access))
+  localStorage.setItem('tokens', JSON.stringify(loginData.tokens))
+}
+
+const getLocalStorageUser = (item = 'user') => {
+  const data = localStorage.getItem(item) ?? null
+  if(data === null){
+    return data
+  }
+  try{
+    return JSON.parse(data)
+  }catch(error){
+    console.error(error)
+  }
 }
 
 export const useUserSessionStore = defineStore('userSession', {
   state: () => ({
-    user: JSON.parse(localStorage.getItem('user') ?? ''),
-    token: JSON.parse(localStorage.getItem('token') ?? ''),
+    user: getLocalStorageUser(),
+    token: getLocalStorageUser('token'),
   }),
   actions: {
     async register(data:registerModel){
-      const user:registerResponse = await axiosInstance.post(`/v1/auth/register`, data);    
-      console.log(user)
+      const user:AxiosResponse<registerResponse> = await axiosInstance.post(`/v1/auth/register`, data)    
 
-      this.user = user;
-      this.token = user.tokens.access;
-      setLoginData(user);
+      if(user.status !== 201){
+        return
+      }
+      this.user = user.data
+      this.token = user.data.tokens.access
+      setLoginData(user.data)
+      this.router.push({name: 'dashboard'})
     },
     async login(data:loginModel) {
-      try{
-        const user:loginResponse = await axiosInstance.post(`/v1/auth/login`, data);    
+        const user:loginResponse = await axiosInstance.post(`/v1/auth/login`, data)    
 
-        this.user = user.user;
-        this.token = user.tokens.access;
-        setLoginData(user);
-      } catch(error){
-        console.error(error)
-      }
-
+        this.user = user.user
+        this.token = user.tokens.access
+        setLoginData(user)
     },
     logout() {
-        this.user = null;
-        this.token = null;
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        localStorage.removeItem('tokens');
+        this.user = null
+        this.token = null
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
+        localStorage.removeItem('tokens')
     }
   }
 })
